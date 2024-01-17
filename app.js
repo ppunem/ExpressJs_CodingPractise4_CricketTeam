6806f1f32f79ee7e7ba122c5beff583d36045286
ppunem <128507331+ppunem@users.noreply.github.com>
2024-01-17 23:43:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,10 +28,58 @@ const initializeDbAndServer = async () => {
 
 initializeDbAndServer()
 
+//1.API to get all players
 app.get('/players/', async (request, response) => {
   const playerDetailsQuery = `
     SELECT * FROM cricket_team
   `
   const playerDetails = await db.all(playerDetailsQuery)
   response.send(playerDetails)
+})
+
+//2.API to add a new player
+app.post('/players/', async (request, response) => {
+  const playerDetails = request.body
+  const {playerName, jerseyNumber, role} = playerDetails
+
+  const addNewPlayerQuery = `
+    INSERT INTO cricket_team (player_name,jersey_number,role) 
+    VALUES ('${playerName}','${jerseyNumber}','${role}')
+  `
+  await db.run(addNewPlayerQuery)
+  response.send('Player Added to Team')
+})
+
+//3.API to get a player based on id
+app.get('/players/:playerId', async (request, response) => {
+  const {playerId} = request.params
+  const getPlayerQuery = `
+    SELECT * FROM cricket_team WHERE player_id='${playerId}'
+  `
+  const playerDetails = await db.get(getPlayerQuery)
+  response.send(playerDetails)
+})
+
+//4.API to update an existing player data
+app.put('/players/:playerId/', async (request, response) => {
+  const {playerId} = request.params
+  const playerDetails = request.body
+  const {playerName, jerseyNumber, role} = playerDetails
+
+  const updateQuery = `
+    UPDATE cricket_team SET player_name='${playerName}',jersey_Number='${jerseyNumber}',role='${role}' 
+    WHERE player_id='${playerId}'
+  `
+  await db.run(updateQuery)
+  response.send('Player Details Updated')
+})
+
+//5.API to delete a player based on id
+app.delete('/players/:playerId/', async (request, response) => {
+  const {playerId} = request.params
+  const deleteQuery = `
+    DELETE from cricket_team WHERE player_id='${playerId}'
+  `
+  await db.run(deleteQuery)
+  response.send('Player Removed')
 })
